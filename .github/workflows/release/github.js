c36a30f15235bdc4cc133999e5be996548b21339
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-01-03 12:36:44 +0100

    Updated Workflow.

---
+++ .github/workflows/release/github.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const package = require('../../../package.json');
+
+package.name = '@dennykorsukewitz/generator-sublime-package';
+
+// Update package.json with the updated name
+fs.writeFileSync(
+  path.join(__dirname, '../package.json'),
+  JSON.stringify(package, null, 2),
+);
